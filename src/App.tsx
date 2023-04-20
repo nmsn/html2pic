93a2db1dccf7a5066346d9891e1ea47cb2e39678
nmsn <136696700@qq.com>
2023-04-20 20:38:48 +0800

    feat: init dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import publicPng from "/public/logo-white.png";
+
+import "./App.css";
+
+const Pic = ({ src }: { src: string }) => {
+  return (
+    <div className="content">
+      <img src={src} className="pic" />
+      <div className="text">测试文本内容</div>
+    </div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <Pic src={publicPng} />
+        <Pic src="https://s2.loli.net/2023/04/20/4t8NgpMxkdVCJcs.png" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
