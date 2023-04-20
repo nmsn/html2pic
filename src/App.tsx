1921873e27435645c61b64111e0d8476ee3ecc73
nmsn <136696700@qq.com>
2023-04-20 21:50:21 +0800

    feat: add html2canvas and html-to-image

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,71 @@
-import publicPng from "/public/logo-white.png";
+import { useRef, useState } from "react";
 
+import publicPng from "/logo-white.png";
+import html2canvas from "html2canvas";
+import * as htmlToImage from "html-to-image";
 import "./App.css";
 
-const Pic = ({ src }: { src: string }) => {
+const Pic1 = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const [imgUrl, setImgUrl] = useState("");
+
+  const getImg = () => {
+    ref?.current &&
+      html2canvas(ref?.current).then((canvas) => {
+        const image = canvas.toDataURL("image/png", 1);
+        setImgUrl(image);
+        console.log(image);
+      });
+  };
   return (
-    <div className="content">
-      <img src={src} className="pic" />
-      <div className="text">测试文本内容</div>
+    <div>
+      <div className="content" ref={ref}>
+        <img src={publicPng} className="pic" />
+        <div className="text">测试文本内容1</div>
+      </div>
+      <div className="btnContainer">
+        <button onClick={getImg} className="btn">
+          确定
+        </button>
+
+        <button onClick={() => setImgUrl("")} className="btn">
+          清空
+        </button>
+      </div>
+
+      {imgUrl && <img src={imgUrl} className="result" />}
+    </div>
+  );
+};
+
+const Pic2 = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [imgUrl, setImgUrl] = useState("");
+
+  const getImg = () => {
+    ref?.current &&
+      htmlToImage.toPng(ref?.current).then((dataUrl) => {
+        setImgUrl(dataUrl);
+        console.log(dataUrl);
+      });
+  };
+  return (
+    <div>
+      <div className="content" ref={ref}>
+        <img src={publicPng} className="pic" />
+        <div className="text">测试文本内容2</div>
+      </div>
+      <div className="btnContainer">
+        <button onClick={getImg} className="btn">
+          确定
+        </button>
+        <button onClick={() => setImgUrl("")} className="btn">
+          清空
+        </button>
+      </div>
+
+      {imgUrl && <img src={imgUrl} className="result" />}
     </div>
   );
 };
@@ -15,8 +74,8 @@ function App() {
   return (
     <>
       <div className="container">
-        <Pic src={publicPng} />
-        <Pic src="https://s2.loli.net/2023/04/20/4t8NgpMxkdVCJcs.png" />
+        <Pic1 />
+        <Pic2 />
       </div>
     </>
   );
